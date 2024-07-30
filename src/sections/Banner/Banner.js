b7f1e79f80dbe9229131431966cf7d3cc1280adf
longvd89 <vulongbka@gmail.com>
2024-07-31 00:19:52 +0700

    update

--- src/sections/Banner/Banner.js
+++ src/sections/Banner/Banner.js
@@ -3,13 +3,13 @@ import './Banner.scss';
 // import {Link} from 'react-router-dom';
 import icon from '../../assets/banner/icons/Calling.png';
 import iconLocation from '../../assets/footer/location.png';
-import bannerImg from '../../assets/dental.webp';
+// import bannerImg from '../../assets/dental.webp';
 // import doctorImg from '../../assets/banner/doctor.png';
 import bannerPattern from '../../assets/banner/pattern_02.png';
 // import shapeOne from '../../assets/banner/vector_01.png';
-import shapeTwo from '../../assets/banner/vector_02.png';
-import shapeThree from '../../assets/banner/vector_03.png';
-import shapeFour from '../../assets/banner/pattern.png';
+// import shapeTwo from '../../assets/banner/vector_02.png';
+// import shapeThree from '../../assets/banner/vector_03.png';
+// import shapeFour from '../../assets/banner/pattern.png';
 
 const InfoComponent = ({ info }) => {
 
@@ -114,73 +114,57 @@ const Banner = () => {
     return (
         <section className='section-bg section-common banner-section'>
             <div className="container pt-40 pb-30">
-                <div className="row">
-                    <div className="col-lg-7">
-                        <div className="banner-text" data-aos="fade-up" data-aos-duration="2000">
-                            <h2>Trung tâm bảo hành Nhất Tín Labo</h2>
-                            <p>Chúng tôi luôn sẵn sàng hỗ trợ quý khách với các dịch vụ bảo hành nhanh chóng và chuyên nghiệp. Mọi thắc mắc vui lòng liên hệ.</p>
-                            <div className="banner-bottom">
-                                <div className="banner-call">
-                                    <div className='icon'>
-                                        <img src={icon} alt="icon" />
-                                    </div>
-                                    <div className='call-text'>
-                                        <p>Hotline</p>
-                                        <h6>02466897699 - 0985243976</h6>
-                                    </div>
+                <div className="banner-text" data-aos="fade-up" data-aos-duration="2000">
+                    <h2 style={{ marginBottom: '16px' }}>Trung tâm bảo hành Nhất Tín Labo</h2>
+                    <p>Chúng tôi luôn sẵn sàng hỗ trợ quý khách với các dịch vụ bảo hành nhanh chóng và chuyên nghiệp. Mọi thắc mắc vui lòng liên hệ.</p>
+                    <div className='search-container' style={{ justifyContent: 'space-around' }}>
+                        <div className="banner-call" style={{ marginRight: '24px' }}>
+                            <div className='item'>
+                                <div className='icon'>
+                                    <img src={icon} alt="icon" />
+                                    
+                                </div> 
+                                <p>Hotline</p>
                                 </div>
+
+                            <div className='call-text'>
+                                <h6>02466897699 - 0985243976</h6>
                             </div>
-                            <div className="banner-bottom">
-                                <div className="banner-call">
-                                    <div className='icon'>
-                                        <img src={iconLocation} alt="icon" />
-                                    </div>
-                                    <div className='call-text'>
-                                        <p>Địa chỉ</p>
-                                        <h6>Số 33, ngõ 69A Hoàng Văn Thái, Thanh Xuân, Hà Nội</h6>
-                                    </div>
+                        </div>
+                        <div className="banner-call">
+                            <div className='item'>
+                                <div className='icon'>
+                                    <img src={iconLocation} alt="icon" />
                                 </div>
-                            </div>
-
-                            <div className="row banner-bottom">
-
-                                <div className="col-lg-8">
-                                    <div class="form-group">
-                                        {/* <label>Name</label> */}
-                                        <input type="text"
-                                            class="form-control"
-                                            placeholder="Nhập mã thẻ bảo hành"
-                                            value={serial}
-                                            onChange={handleChange}
-                                        />
-                                    </div>
-                                </div>
-
-                                <div className="col-lg-4">
-                                    <button className='button-search' onClick={handleSearch}>Tra cứu</button>
-                                </div>
-
+                                <p>Địa chỉ</p>
 
                             </div>
-
+                            
+                            <div className='call-text'>
+                                
+                                <h6>Số 33, ngõ 69A Hoàng Văn Thái, Thanh Xuân, Hà Nội</h6>
+                            </div>
                         </div>
                     </div>
 
-                    <div className="col-lg-5">
-                        <div className="banner-img-area" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="500">
-                            <div className="banner-img">
-                                <img src={bannerImg} alt="banner model" />
-                            </div>
+                    <div className='search-container'>
 
-                            {/* Vector Shapes */}
-                            <div className="shapes">
-                                {/* <img src={shapeOne} alt="shape" /> */}
-                                <img src={shapeTwo} alt="shape" />
-                                <img src={shapeThree} alt="shape" />
-                                <img src={shapeFour} alt="shape" />
-                            </div>
+                        <div class="form-group search-text">
+                            {/* <label>Name</label> */}
+                            <input type="text"
+                                class="form-control"
+                                placeholder="Nhập mã thẻ bảo hành"
+                                value={serial}
+                                onChange={handleChange}
+                            />
                         </div>
+
+                        <button className='button-search' onClick={handleSearch}>Tra cứu</button>
+
                     </div>
+
+
+
                 </div>
 
                 <div className="service-box">
